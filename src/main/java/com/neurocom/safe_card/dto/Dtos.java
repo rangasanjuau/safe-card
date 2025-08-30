8f7757e2994c5d303f8acff7c9c6908df1633c2f
Sanjay Ranga <sanjay.ranga.au@gmail.com>
2025-08-30 18:09:22 +1000

    Write Request and Response Objects

---
+++ src/main/java/com/neurocom/safe_card/dto/Dtos.java
@@ -0,0 +1,15 @@
+package com.neurocom.safe_card.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class Dtos {
+
+    // Request Object
+    public record CardRequest(String cardholderName, String pan) {
+    }
+
+    // Response Object
+    public record CardView(UUID id, String cardholderName, String maskedPan, Instant creationTime) {
+    }
+}
