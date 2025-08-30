b2f85c25f41e34e907fd771f028ccfcbc2a3d009
Sanjay Ranga <sanjay.ranga.au@gmail.com>
2025-08-30 17:18:15 +1000

    Initial Commit - Initialised springboot application

---
+++ src/main/java/com/neurocom/safe_card/SafeCardApplication.java
@@ -0,0 +1,13 @@
+package com.neurocom.safe_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neurocom/safe_card/SafeCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurocom.safe_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
